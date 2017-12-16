971b799b1ff7ded303ec65e0b6ccd8a3358ef99d
erincdemir <erincguray@gmail.com>
2017-12-16 07:02:27 +0300

    express ve mongoose ile yeniden api yazıldı

--- api/backend.js
+++ api/backend.js
@@ -1,230 +1,63 @@
-//mongodb test
-var MongoClient = require('mongodb').MongoClient;
-var dburl = "mongodb://localhost:27017/test";
-//test
-const http = require('http');
-const url = require('url');
-const querystring = require('querystring');
+var express = require('express');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var User = require('./User');
+var session = require('express-session');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var app = express();
 
-const server = http.createServer((req, res) => {
- res.writeHead(200, {
-    "Content-Type": "text/plain",
-    "Access-Control-Allow-Origin":"*"
+mongoose.connect('mongodb://localhost/test'),{useMongoClient:true};
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false
+}));
+
+app.get('/', function(req, res){
+  res.send('hello world');
 });
-      if(req.method == 'POST'){
-          var body = '';
-            
-           req.on('data', function (data) {
-            body += data;
-            });
-            req.on('end', function () {
-                 var post = querystring.parse(body);
-                //mongo db post handler
-                MongoClient.connect(dburl, function(err, db) {
-                    var testmethod = post.method;
-                    var testusr=post.usr;
-                    var testpwd=post.pwd;
-                    var status=post.stat;
-                    var testarray= post.array; 
-                    var school = post.school;
-                    var year = post.year;
-                    var department = post.department;
-                    var score = post.score;
-                    var word = post.word;
-  if (err) throw err;
-                    
-if(testmethod == "login"){
-  db.collection("test").findOne({"usr":testusr}, 
-function(err, result) {
-    if (err) throw err;
-      
-        if(testpwd == result.pwd){
-            res.end("Hoşgeldin "+testusr+"!");
-            db.collection("test").update({usr:testusr}, {$set: {status:"online",modified:new Date()}});
-            
-        }
-      else
-          {
-              res.end("Kullanıcı adı veya şifre hatalı");
-              
-          }
-      
-    db.close();
+
+app.post('/kayit', function(req, res){
+  if (req.body.email &&
+  req.body.username &&
+  req.body.password &&
+  req.body.passwordConf) {
+  var userData = {
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    passwordConf: req.body.passwordConf,
+  }
+  //use schema.create to insert data into the db
+  User.create(userData, function (err, user) {
+    if (err) {
+      return next(err)
+    } else {
+      return res.send('Kullanıcı kaydedildi');
+    }
   });
 }
-else if(testmethod == "logoff"){
-    res.end(testusr+" kullanıcısından çıkış yaptınız.");
-    var logintime;
-    db.collection("test").findOne({usr:testusr},function(err,result){
-        if(err) throw err;
-        logintime = result.modified.getTime();
-        db.collection("test").update({usr:testusr}, {$set: {status:"offline",modified:new Date()}});
-    });
-    
-      db.collection("test").findOne({usr:testusr},function(err,result){
-        if(err) throw err;
-        var screentime = new Date().getTime() - logintime;
-        if(result.screentime == undefined){
-          db.collection("test").update({usr:testusr}, {$set: {screentime:screentime}});  
-        }
-        else{
-            screentime += result.screentime;
-            db.collection("test").update({usr:testusr}, {$set: {screentime:screentime}});  
-        }
-        db.close();
-    });
-   
-}
-else if(testmethod == "usrstatus"){
-    db.collection("test").findOne({"usr":testusr},function(err,result){
-                   if(err) throw err;
-                   res.end(result.status);
-                   db.close();                                     
-                                                            });
-}
-else if(testmethod == "lastseen"){
-     db.collection("test").findOne({"usr":testusr},function(err,result){
-                   if(err) throw err;
-                   res.end(new Date(result.modified).toDateString() +" "+ new Date(result.modified).getHours() + ":" + new Date(result.modified).getMinutes());
-                   db.close();                                     
-                                                            }); 
-        
-}
-else if(testmethod == "wordlist_add"){
-
-
-var remoteArray="";
-var knownwords = 0;
-
-db.collection("test").findOne({"usr":testusr},function(err,result){
-                   if(err) throw err;
-                   if(result.words == undefined){
-                    db.collection("test").update({usr:testusr}, {$set:{words : {0:""}}});    
-                   }
-                  // db.close();                                     
-                                                            }); 
-db.collection("test").findOne({"usr":testusr},function(err,result){
-    if(err) throw err;
-    remoteArray=JSON.parse(testarray);
-    if(result.wordcount != undefined){
-        knownwords = result.wordcount;
-    }
-    for(var j= 0;j<remoteArray.length;j++){
-       var obj = {};
-       obj["words."+(knownwords+j)] = remoteArray[j];
-    db.collection("test").update({usr:testusr}, {$set:obj});
-    }
-    var updatedwords = knownwords+remoteArray.length;
-    db.collection("test").update({usr:testusr}, {$set:{wordcount:updatedwords}});
-    
-    res.end("Sözcükler eklendi.");
-db.close();    
-});
-            
-    
-}
-else if(testmethod == "wordlist_show"){
-    db.collection("test").findOne({"usr":testusr},function(err,result){
-                   if(err) throw err;
-                    res.end(JSON.stringify(result.words));
-                    db.close();                                     
-                                                            }); 
-}
-else if(testmethod == "update_education"){
-db.collection("test").findOne({"usr":testusr},function(err,result){
-                   if(err) throw err;
-                   if(result.department == undefined){
-                      db.collection("test").update({usr:testusr}, {$set:{department : ""}});    
-                   }
-                   if(result.school == undefined){
-                      db.collection("test").update({usr:testusr}, {$set:{school : ""}});    
-                   }
-                   if(result.year == undefined){
-                      db.collection("test").update({usr:testusr}, {$set:{year : ""}});    
-                   }
-                  // db.close();                                     
-                                                            }); 
-db.collection("test").findOne({"usr":testusr},function(err,result){
-    if(err) throw err;
-    if(school != undefined){
-         db.collection("test").update({usr:testusr}, {$set:{school:school}});
-    }
-     if(year != undefined){
-         db.collection("test").update({usr:testusr}, {$set:{year:parseInt(year)}});
-    }
-    if(department != undefined){
-         db.collection("test").update({usr:testusr}, {$set:{department:department}});
-    }    
-    db.close(); 
-    res.end("Eğitim bilgileri güncellendi.");
-});
-           
-        
-}
-else if(testmethod == "score_add"){
-    db.collection("test").findOne({usr:testusr},function(err,result){
-                   if(err) throw err;
-                   if(!result.score){
-                      db.collection("test").update({usr:testusr}, {$set:{score : "0"}});    
-                   }                                   
-                                                            }); 
-db.collection("test").findOne({usr:testusr},function(err,result){
-    if(err) throw err;
-    if(score != undefined){
-         var newScore = parseInt(result.score) + parseInt(score);
-         db.collection("test").update({usr:testusr}, {$set:{score:newScore.toString()}});
-         res.end("Skor eklendi.");
-    }
-    db.close();    
-});
-          
-}
-else if(testmethod=="register"){
-    db.collection("test").findOne({usr:testusr},function(err,result){
-        if(err) throw err;
-        if(result){
-            res.end("Kullanıcı adı mevcut.");
-        }else{
-            db.collection("test").insertOne({usr:testusr,pwd:testpwd});
-            res.end("Kullanıcı eklendi.");  
-        }
-        db.close();
-    });
-   
-}
-else if(testmethod=="add_word"){
-    db.collection("words").findOne({word:word},function(err,result){
-        if(err) throw err;
-        if(result){
-            var obj = {};
-            obj["who_searched."+testusr] = testusr;
-            db.collection("words").update({word:word}, {$set:obj});
-            res.end("sözcük bulundu.");
-        }
-        else{
-            var obj = {};
-            obj[testusr] = testusr;
-            db.collection("words").insertOne({word:word,who_searched:obj});
-            res.end("sözcük eklendi.");
-        }
-        db.close();
-    });
-}
-});
-               
-                
-            });
-      }   
-      if(req.method == 'GET'){
-          res.write("Bu bir get requesttir\n");
-          res.end("cukum\n");
-      }
-  
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.post('/giris',function(req,res){
+	if(req.body.email && 
+	   req.body.password){
+	   User.authenticate(req.body.email, req.body.password, function(error,user){
+	   		if(error|| !user){
+	   			res.send('Kullanıcı adı veya şifre hatalı.');
+	   		}else{
+	   			req.session.userId = user._id;
+	   			res.send(user.username + ' olarak giriş yapıldı.');
+	   		}
+	   });	
+	}
+
 });
+
+
+
+app.listen(8080);
