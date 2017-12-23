959f5cfca398c581285055445e8728f81c29e854
deneme <kutlumahmut688@gmail.com>
2017-12-24 02:22:05 +0300

    admin sayfaları oluşturuldu
    kullanıcı ekle ve kullanıcılar sayfası tamamlandı

---
+++ akademi önyüz/akademi html/script.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+console.log("connectedbitch")
+
+
+
+
+
+
+function myFunction(){
+
+ console.log("ananı sikiym");
+
+ $('#ebenisikim')
+  .sidebar('toggle')
+;
+
+
+};
+/*
+$('.ui.sidebar')
+  .sidebar('toggle')
+;
+document.getElementById("navbar").onclick = function(console.log();
+
+*/
